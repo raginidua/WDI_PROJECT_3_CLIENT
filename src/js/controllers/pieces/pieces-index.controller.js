5db3eebd657e8967d1f050f20c08f939d45cfc32
raginidua <raginidua1@gmail.com>
2017-02-19 18:45:47 +0000

    Updated to ng-resource

--- src/js/controllers/pieces/pieces-index.controller.js
+++ src/js/controllers/pieces/pieces-index.controller.js
@@ -2,18 +2,28 @@ angular
 .module('artApp')
 .controller('PiecesIndexCtrl', PiecesIndexCtrl);
 
-PiecesIndexCtrl.$inject = ['API', '$http'];
-function PiecesIndexCtrl(API, $http) {
+PiecesIndexCtrl.$inject = ['API', '$http', '$resource'];
+function PiecesIndexCtrl(API, $http, $resource) {
   const vm = this;
 
-  $http({
-    method: 'get',
-    url: 'http://localhost:3000/pieces'
-  }).then(response => {
-    console.log(response.data);
-    vm.pieces = response.data;
-    console.log(vm.pieces[0].title);
-  }, err => {
-    console.log(err);
-  });
+  const Piece = $resource(`${API}/pieces/:id`,
+    { id: '@_id' }
+  );
+
+  piecesIndex();
+
+  function piecesIndex(){
+    vm.pieces = Piece.query();
+  }
+
+  // $http({
+  //   method: 'get',
+  //   url: 'http://localhost:3000/pieces'
+  // }).then(response => {
+  //   console.log(response.data);
+  //   vm.pieces = response.data;
+  //   console.log(vm.pieces[0].title);
+  // }, err => {
+  //   console.log(err);
+  // });
 }
--- src/js/controllers/pieces/pieces-show.controller.js
+++ src/js/controllers/pieces/pieces-show.controller.js
@@ -2,17 +2,22 @@ angular
 .module('artApp')
 .controller('PiecesShowCtrl', PiecesShowCtrl);
 
-PiecesShowCtrl.$inject = ['API', '$http', '$stateParams'];
-function PiecesShowCtrl(API, $http, $stateParams) {
+PiecesShowCtrl.$inject = ['API', '$http', '$stateParams', '$resource'];
+function PiecesShowCtrl(API, $http, $stateParams, $resource) {
   const vm = this;
 
-  $http({
-    method: 'get',
-    url: `http://localhost:3000/pieces/${$stateParams.id}`
-  }).then(response => {
-    console.log(response.data);
-    vm.piece = response.data;
-  }, err => {
-    console.log(err);
-  });
+  const Piece = $resource(`${API}/pieces/:id`,
+    { id: '@_id' });
+
+  vm.piece = Piece.get($stateParams);
+  //
+  // $http({
+  //   method: 'get',
+  //   url: `http://localhost:3000/pieces/${$stateParams.id}`
+  // }).then(response => {
+  //   console.log(response.data);
+  //   vm.piece = response.data;
+  // }, err => {
+  //   console.log(err);
+  // });
 }
